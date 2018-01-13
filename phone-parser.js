e6f29701829fc399d1b830d6b791d2357e6bc22f
Yong Bakos <ybakos@humanoriented.com>
2018-01-13 12:34:42 -0800

    Test extractPhoneNumberDigits.

--- phone-parser.js
+++ phone-parser.js
@@ -3,7 +3,7 @@
 // extractPhoneNumberDigits("(555)-123-1234") -> 5551231234
 // extractPhoneNumberDigits("1 (555)-123-1234") -> 5551231234
 function extractPhoneNumberDigits(text) {
-
+    return text.match(/\d/g).join([]);
 }
 
 module.exports = extractPhoneNumberDigits;
--- phone-parser.test.js
+++ phone-parser.test.js
@@ -3,3 +3,8 @@ const extractPhoneNumberDigits = require('./phone-parser');
 test("Extracts 1234567890 from '(123) 456-7890'", () => {
   expect(extractPhoneNumberDigits("(123) 456-7890")).toBe("1234567890");
 });
+
+test("Extracts 1234567890 from '123-456-7890'", () => {
+  expect(extractPhoneNumberDigits("123-456-7890")).toBe("1234567890");
+});
+
